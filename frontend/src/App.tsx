5e24ac63f4510f6c47c18a6d6ceb9572ee888155
shubham1995jain <shubhamjain.mmu@gmail.com>
2025-10-28 12:38:06 +0530

    fix browser issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import GetIdCard from "./components/GetIdCard"
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<GetIdCard />} />
+      <Route path="*" element={<GetIdCard />} />
       <Route path="/verify/:dynamicId" element={<Verify />} />
       <Route path="/card/:dynamicId" element={<Card />} />
     </Routes>
